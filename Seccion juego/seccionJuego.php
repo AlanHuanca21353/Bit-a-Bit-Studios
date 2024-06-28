42915b9ef8926453a8e002fbc889918c5cae6cfe
MarcoMirabal <132933666+MarcoMirabal@users.noreply.github.com>
2024-06-28 10:43:56 -0300

    28/06 - 2

---
+++ Seccion juego/seccionJuego.php
@@ -0,0 +1,27 @@
+<?php 
+  include("template/header.php");
+?>
+
+
+<div
+  class="container"
+>
+  <div class="row">
+
+  <div class="p-5 mb-4 bg-light rounded-5">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+      <p class="col-md-8 fs-4">
+        Using a series of utilities, you can create this jumbotron, just
+        like the one in previous versions of Bootstrap. Check out the
+        examples below for how you can remix and restyle it to your liking.
+      </p>
+      <button class="btn btn-primary btn-lg rounded-5" type="button">
+        Example button
+      </button>
+    </div>
+  </div>
+  
+<?php
+  include("template/footer.php");
+?>
