7fffa75bac1268c67a5e5062a7af64596d3c08a0
MarcoMirabal <marcomirabal.9@gmail.com>
2024-08-16 10:39:13 -0300

    16/08/2024

--- Seccion juego/seccionJuego.php
+++ Seccion juego/seccionJuego.php
@@ -4,24 +4,32 @@
 
 
 
+
+
+  
+
+
 <div
   class="container"
 >
   <div class="row">
 
-  <div class="p-5 mb-4 bg-light rounded-5" id="fondo">
+  <div class="p-4 mb-4 bg-light rounded-5" id="fondo">
     <div class="container-fluid py-3" alt=""> 
    
       <h1 class="display-7 fw-bold">¿Serás capaz de salir del bosque?</h1>
       <p class="col-md-8 fs-2">
         ¡Ayuda a nuez en esta nueva aventura!
       </p>
-      <button class="btn btn-primary btn-lg rounded-5 p-4" type="button">
+      <div class="card mb-3 rounded-5">
+  <img class="rounded-5" src="../Seccion Juego/img/fondo_juego.png" class="card-img-top" alt="..." >
+</div>
+  <div class="card-body">
+  <button class="btn btn-lg rounded-5 p-4" type="button" style="width:100%; background-color: #2D14EE; color: #ffffff;">
         Descargalo aquí
       </button>
-    </div>
-  </div>
 
+ 
 
 
 
