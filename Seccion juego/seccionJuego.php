64b2a00d7158fe1cba16712a17373da60851ae5f
MarcoMirabal <marcomirabal.9@gmail.com>
2024-08-16 12:02:30 -0300

    16/08/2024

--- Seccion juego/seccionJuego.php
+++ Seccion juego/seccionJuego.php
@@ -10,11 +10,11 @@
 
 
 <div
-  class="container"
+  class="container"  style="padding:35px; ";
 >
-  <div class="row">
 
-  <div class="p-4 mb-4 bg-light rounded-5" id="fondo">
+
+  <div class="p-4 mb-4 bg-light rounded-5" >
     <div class="container-fluid py-3" alt=""> 
    
       <h1 class="display-7 fw-bold">¿Serás capaz de salir del bosque?</h1>
@@ -22,16 +22,48 @@
         ¡Ayuda a nuez en esta nueva aventura!
       </p>
       <div class="card mb-3 rounded-5">
-  <img class="rounded-5" src="../Seccion Juego/img/fondo_juego.png" class="card-img-top" alt="..." >
+  <img id="imagen" class="rounded-5" src="../Seccion Juego/img/fondo_bosque.png" class="card-img-top" alt="..." >
 </div>
   <div class="card-body">
   <button class="btn btn-lg rounded-5 p-4" type="button" style="width:100%; background-color: #2D14EE; color: #ffffff;">
         Descargalo aquí
       </button>
 
- 
+</div>
 
 
+<div class="container2">
+
+
+<div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+
+</div>
 
 
 
