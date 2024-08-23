6d104538538f5038225008d5531c4834324fe945
MarcoMirabal <132933666+MarcoMirabal@users.noreply.github.com>
2024-08-23 10:42:59 -0300

    23/08/2024
    Terminación de toda la página (faltan algunos detalles y el footer)

--- Seccion juego/seccionJuego.php
+++ Seccion juego/seccionJuego.php
@@ -10,58 +10,77 @@
 
 
 <div
-  class="container"  style="padding:35px; ";
+  class="container"  style="padding:35px;";
 >
 
 
-  <div class="p-4 mb-4 bg-light rounded-5" >
-    <div class="container-fluid py-3" alt=""> 
+  <div class="p-4 mb-4 bg-light rounded-5"  >
+    <div class="container-fluid py-3" alt="" > 
    
       <h1 class="display-7 fw-bold">¿Serás capaz de salir del bosque?</h1>
       <p class="col-md-8 fs-2">
         ¡Ayuda a nuez en esta nueva aventura!
       </p>
       <div class="card mb-3 rounded-5">
-  <img id="imagen" class="rounded-5" src="../Seccion Juego/img/fondo_bosque.png" class="card-img-top" alt="..." >
+  <img class="rounded-5" src="../Seccion Juego/img/fondo_bosque.png" class="card-img-top" alt="..." >
 </div>
   <div class="card-body">
-  <button class="btn btn-lg rounded-5 p-4" type="button" style="width:100%; background-color: #2D14EE; color: #ffffff;">
-        Descargalo aquí
+
+  <h2>  ¿Cómo descargarlo y jugar?  <button class="btn btn-lg rounded-5 p-3" type="button" style="background-color: #2D14EE; color: #ffffff; margin-left: 499px">
+        Descargar
       </button>
-
+</h2>
 </div>
 
 
-<div class="container2">
 
 
-<div class="card-group">
+
+
+
+
+
+
+<div class="container2" >
+
+
+<div class="card-group" style="padding: 10px;">
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75"  class="bi bi-1-circle-" viewBox="0 0 16 16" style="margin: auto; margin-top: 20px">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312z"/>
+</svg>
+
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      <h5 class="card-title" >Abir el instalador</h5>
+      <p class="card-text">Pulsa el boton "Descargar"</p>
+     
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" class="bi bi-2-circle-fill" viewBox="0 0 16 16" style="margin: auto; margin-top: 20px">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.646 6.24c0-.691.493-1.306 1.336-1.306.756 0 1.313.492 1.313 1.236 0 .697-.469 1.23-.902 1.705l-2.971 3.293V12h5.344v-1.107H7.268v-.077l1.974-2.22.096-.107c.688-.763 1.287-1.428 1.287-2.43 0-1.266-1.031-2.215-2.613-2.215-1.758 0-2.637 1.19-2.637 2.402v.065h1.271v-.07Z"/>
+</svg>
+
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">Elije la ruta de instalación</h5>
+      <p class="card-text">Elije donde instalar el juego. Por defecto puedes aceptar la ruta predeterminada de tu equipo.</p>
+     
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" class="bi bi-3-circle-fill" viewBox="0 0 16 16" style="margin: auto; margin-top: 20px">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-8.082.414c.92 0 1.535.54 1.541 1.318.012.791-.615 1.36-1.588 1.354-.861-.006-1.482-.469-1.54-1.066H5.104c.047 1.177 1.05 2.144 2.754 2.144 1.653 0 2.954-.937 2.93-2.396-.023-1.278-1.031-1.846-1.734-1.916v-.07c.597-.1 1.505-.739 1.482-1.876-.03-1.177-1.043-2.074-2.637-2.062-1.675.006-2.59.984-2.625 2.12h1.248c.036-.556.557-1.054 1.348-1.054.785 0 1.348.486 1.348 1.195.006.715-.563 1.237-1.342 1.237h-.838v1.072h.879Z"/>
+</svg>
+
+  <div class="card-body">
+    <h5 class="card-title">Espere y... ¡A jugar!</h5>
+    <p class="card-text">Una vez descargados todos los archivos necesarios, podrás jugar sin problemas. ¡Diviértete!</p>
+   
   </div>
 </div>
-
 
 </div>
 
